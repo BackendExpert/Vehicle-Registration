fad3ba6a39b83b5822cabdd6b358a41aecaf02f7
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-07-21 07:20:12 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -60,7 +60,8 @@
     .conetent2-body {
         padding: 40px 20px;
     }
-    .btn-login{
+
+    .btn-login {
         margin-top: 20px;
         padding: 15px 35px;
         border: 2px solid orange;
@@ -70,13 +71,15 @@
         transition: 0.5s;
         font-size: 20px;
     }
-    .btn-login:hover{
+
+    .btn-login:hover {
         cursor: pointer;
         color: black;
         background: orange;
         padding: 20px 40px;
     }
-    .content2{
+
+    .content2 {
         padding-right: 12px;
         padding-left: 12px;
         margin-right: auto;
@@ -177,15 +180,48 @@
                 <div class="content2-title">
                     Latest Update
                 </div>
-                .
+                <div class="conetent2-body">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem numquam laboriosam assumenda saepe
+                    officia? Facere, dolor corporis porro consectetur illum quidem fuga a minima aliquam laborum ad
+                    soluta nulla saepe!
+                    Blanditiis eaque placeat quisquam vero veritatis assumenda quasi culpa deserunt quam voluptas, fuga
+                    doloribus doloremque voluptatibus ab quo, perspiciatis necessitatibus perferendis? Totam quia eos at
+                    repellat quis cumque reprehenderit minus.
+                    Nisi numquam dignissimos libero vero rerum, quo, perferendis quod culpa voluptatum voluptate
+                    veritatis! Repellendus, quibusdam cum consequuntur nam quas id laborum vitae molestias sint ducimus
+                    maxime incidunt dolore, odit libero?
+                    Perferendis distinctio, vitae, pariatur suscipit necessitatibus consectetur exercitationem facilis
+                    assumenda odit neque non illum debitis. Ratione exercitationem aspernatur voluptas at quisquam odio
+                    aut consequuntur dolor enim. Nemo molestias cum quas?
+                    Necessitatibus ipsa, cum quibusdam molestias est architecto totam? Maxime consequatur illo molestias
+                    nobis! Nobis dolor sed reiciendis. Accusamus sit temporibus officia quidem alias praesentium,
+                    quibusdam eaque ducimus pariatur asperiores iste.
+                </div>
             </div>
             <div class="col-lg-6 features">
                 <div class="content2-title">
                     Latest Update
                 </div>
+                <div class="conetent2-body">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, minima necessitatibus accusantium
+                    impedit eum dolor magnam eos recusandae dolores deleniti ea est et nisi consectetur. Numquam
+                    incidunt error reiciendis suscipit.
+                    Perspiciatis, quaerat. Unde maxime saepe, fuga facere laboriosam aspernatur eaque totam expedita
+                    ipsam dicta rem voluptatum adipisci dolorem quo excepturi consequuntur obcaecati quidem possimus
+                    quaerat ut pariatur quod. Provident, ullam!
+                    Fugit commodi nobis, ad cumque exercitationem ut delectus id consequatur deleniti excepturi.
+                    Praesentium labore et architecto quidem esse iste nostrum sunt saepe ipsum a expedita, iusto
+                    dignissimos omnis ratione cumque.
+                    Aliquam adipisci neque repudiandae expedita doloribus! Obcaecati earum, quod odio nihil aspernatur
+                    itaque amet a dignissimos eveniet, odit officiis quae, expedita in voluptate fuga reprehenderit
+                    tenetur animi qui. Voluptas, assumenda!
+                    Enim quasi totam, molestias provident sunt sit, ex ad, id eligendi quibusdam dicta aperiam magni.
+                    Natus esse perferendis, velit voluptates, autem officia impedit earum iusto odit vero quasi sint
+                    voluptate?
+                </div>
             </div>
         </div>
-    
+
     </div>
 
 </body>
