5e8d51467f13649a08d0d029bc4b8d371c164956
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-07-22 09:27:35 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -35,6 +35,7 @@
     }
     input[type=email]:focus{
         border: 2px solid #6a74ff;
+        box-shadow: 10px 10px 8px 10px #888888;
     }
     input[type=password]:focus{
         border: 2px solid #6a74ff;
