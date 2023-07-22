24ceec66924dd0115d063aa168610d6afbd11385
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-07-22 16:34:21 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- resources/views/vehicles/show.blade.php
+++ resources/views/vehicles/show.blade.php
@@ -103,7 +103,9 @@
     @extends('layouts.dash')
 
     @section('content')
-        
+        <div class="admin-card">
+            <a href="{{ url('/vehicles') }}"></a>
+        </div>
     @endsection
 
 @endif
