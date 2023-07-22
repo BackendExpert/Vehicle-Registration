514319ab18fbdd21b00a732f86a3f9c33d90cc49
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-07-22 13:12:18 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- app/Models/VehiclesM.php
+++ app/Models/VehiclesM.php
@@ -9,6 +9,6 @@ class VehiclesM extends Model
 {
     protected $table = 'vehicles';
     protected $primaryKey = 'id';
-    protected $fillable = ['vehicle_plate_no', 'vehicle_chassis_no', 'vehicle_registed_no', 'owner', 'vehicle_type', 'vehicle_model', 'created_at', 'updated_at'];
+    protected $fillable = ['vehicle_plate_no', 'vehicle_chassis_no', 'vehicle_registed_no', 'owner', 'vehicle_type', 'vehicle_model', 'vehicle_color', '', 'created_at', 'updated_at'];
     use HasFactory;
 }
