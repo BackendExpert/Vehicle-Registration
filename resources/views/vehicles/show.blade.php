e03f6df86a24e06dc9aae309d3d752baadc8a7a4
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-07-22 15:58:09 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- resources/views/vehicles/show.blade.php
+++ resources/views/vehicles/show.blade.php
@@ -8,12 +8,12 @@
 
 <div class="container">
     <div class="row">
-        <div class="col-lg-4"></div>
-        <div class="col-lg-4">
+        <div class="col-lg-3"></div>
+        <div class="col-lg-6">
             <div class="print-title">
                 <h1>Online Vehicle Registration Application</h1>
             </div>            
         </div>
-        <div class="col-lg-4"></div>
+        <div class="col-lg-3"></div>
     </div>
 </div>
