03a6482f2067e402b43d594c0fe85069e8cd9246
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-07-22 09:33:35 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -88,6 +88,7 @@
                                 </span>
                             @enderror
                         
+                            <br><br>
 
                             <div class="form-check">
                                 <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
