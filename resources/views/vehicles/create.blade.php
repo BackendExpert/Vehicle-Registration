15f3c820b712a745f71bd52889bb141201ad4f0d
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-07-22 13:55:21 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- resources/views/vehicles/create.blade.php
+++ resources/views/vehicles/create.blade.php
@@ -2,6 +2,6 @@
 
 @section('content')
     <div class="admin-card">
-        <a href="{{ url('/vehicles') }}"></a>
+        <a href="{{ url('/vehicles') }}"><button class="btn btn-primary">Back</button></a>
     </div>
 @endsection
