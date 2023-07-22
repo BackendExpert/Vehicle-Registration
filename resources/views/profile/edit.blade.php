f83eb4b0331ef57495fd740d58d5262cf7faa86c
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-07-22 12:17:37 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -9,6 +9,6 @@
         <h3><i class="fas fa-user-edit"></i> Update {{ $member_profile->name }}'s Profile</h3>
         <hr>
 
-        <form action="" method="post"></form>
+        <form action="{{ url('users/' .$member_profile->id) }}" method="POST"></form>
     </div>
 @endsection
