c712749a842617274fb9d330d37f572adc6a9730
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-07-22 14:02:30 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- resources/views/vehicles/index.blade.php
+++ resources/views/vehicles/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="admin-card">
-        <h3> <i class="fas fa-shuttle-van"></i> Registed Vehicles</h3>
+        <h3> <i class="fas fa-shuttle-van"></i> Registered Vehicles</h3>
         <hr>
 
         <a href="{{ url('vehicles/create') }}"><button class="btn btn-success"><i class="fas fa-plus"></i> Register New Vehicle</button></a>
