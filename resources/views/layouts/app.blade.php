a13205b3b57254046206be9d14ea6b0e91650d4f
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-07-22 09:15:41 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -57,6 +57,9 @@
                                 <li class="nav-item">
                                     <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                                 </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                </li>
                             @endif
 
                         @else
