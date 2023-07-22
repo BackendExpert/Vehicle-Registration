00df875ea8c3173c7597236f0b737f07598a02d0
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-07-22 08:44:31 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -26,16 +26,21 @@
 @section('content')
     <div class="login-content">
         <div class="container">
-            <div class="login-here">
-                <span class="main-title">{{__('Login Here')}}</span>
-                <form action="{{ route('login') }}" method="POST">
-                @csrf
-        
-                    <label for="email">{{__('Email Address')}}</label>
-        
-                </form>
-            </div>
-    
+            <div class="row">
+                <div class="col-lg-4"></div>
+                <div class="col-lg-4">
+                    <div class="login-here">
+                        <span class="main-title">{{__('Login Here')}}</span>
+                        <form action="{{ route('login') }}" method="POST">
+                        @csrf
+                
+                            <label for="email">{{__('Email Address')}}</label>
+                
+                        </form>
+                    </div>
+                </div>
+                <div class="col-lg-4"></div>
+            </div>    
         </div>
     </div>
 
