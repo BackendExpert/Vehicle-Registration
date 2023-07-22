567ee3c58a1b8e64fe6888e7d66ff379af5b4aa0
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-07-22 13:01:58 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

---
+++ app/Models/VehiclesM.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class VehiclesM extends Model
+{
+    use HasFactory;
+}
