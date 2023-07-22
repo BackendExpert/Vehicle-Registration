9dccc60b89aa07273fdb277150e98f2ea9e23079
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-07-22 15:05:45 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- resources/views/vehicles/index.blade.php
+++ resources/views/vehicles/index.blade.php
@@ -30,7 +30,7 @@
                         <td>{{ $vehicle->vehicle_type }}</td>
                         <td>{{ $vehicle->created_at }}</td>
                         <td>
-                            <a href="{{ url('/vehicles/' . $vehicle->id) }}" title="View videos "><button class="btn btn-info btn-sm"><i class="fa fa-eye" aria-hidden="true"></i> View</button></a>
+                            <a href="{{ url('/vehicles/' . $vehicle->id) }}" title="View Vehicle "><button class="btn btn-info btn-sm"><i class="fa fa-eye" aria-hidden="true"></i> View</button></a>
                             
                             @if (auth()->user()->role == 1 || auth()->user()->role == 2 || auth()->user()->name == $vid->uploader)
                                 <a href="{{ url('/vehicles/' . $vehicle->id . '/edit') }}" title="Edit Vehicle"><button class="btn btn-primary btn-sm"><i class="fas fa-edit" aria-hidden="true"></i> Edit</button></a>
@@ -40,7 +40,7 @@
                                 <form method="POST" action="{{ url('/vehicles' . '/' . $vehicle->id) }}" accept-charset="UTF-8" style="display:inline">
                                     {{ method_field('DELETE') }}
                                     {{ csrf_field() }}
-                                    <button type="submit" class="btn btn-danger btn-sm" title="Delete video" onclick="return confirm(&quot;Confirm delete?&quot;)"><i class="fas fa-calendar-times" aria-hidden="true"></i> Delete</button>
+                                    <button type="submit" class="btn btn-danger btn-sm" title="Delete Vehicle" onclick="return confirm(&quot;Confirm delete?&quot;)"><i class="fas fa-calendar-times" aria-hidden="true"></i> Delete</button>
                                 </form>                            
                             @endif                            
                         </td>
