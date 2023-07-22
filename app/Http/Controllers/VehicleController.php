c089c5fcc3b72f23d16d7e366b01afb6d2368a54
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-07-22 17:17:30 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- app/Http/Controllers/VehicleController.php
+++ app/Http/Controllers/VehicleController.php
@@ -78,7 +78,24 @@ class VehicleController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $this->validate($request, [
+            'vehicle_plate_no' => ['required', 'min:10', 'max:11', 'unique:vehicles'],
+            'vehicle_chassis_no' => ['required', 'min:12', 'max:15', 'unique:vehicles'],
+            'vehicle_type' => ['required', 'min:2', 'max:10'],
+            'vehicle_model' => ['required', 'min:1', 'max:30'],
+            'vehicle_color' => ['required', 'min:2', 'max:10'],
+            'seats' => ['required'],
+        ]);
+
+        $new_vechicle = new VehiclesM();
+        $new_vechicle->vehicle_plate_no = $request->vehicle_plate_no;
+        $new_vechicle->vehicle_chassis_no = $request->vehicle_chassis_no;
+        $new_vechicle->vehicle_type = $request->vehicle_type;
+        $new_vechicle->vehicle_model = $request->vehicle_model;
+        $new_vechicle->vehicle_color = $request->vehicle_color;
+        $new_vechicle->seats = $request->seats;
+
+        dd($new_vechicle);
     }
 
     /**
