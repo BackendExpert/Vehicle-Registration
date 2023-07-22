f2e02f3aa3a6cfdd5e226651e2511b89e0892986
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-07-22 17:00:23 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- resources/views/vehicles/edit.blade.php
+++ resources/views/vehicles/edit.blade.php
@@ -2,6 +2,8 @@
 
 @section('content')
     <div class="admin-card">
-        
+        <a href="{{ url('/vehicales') }}"><button class="btn btn-primary">Back</button></a>
+        <br>
+        <h3>Update Information on Vehicle Number </h3>
     </div>
 @endsection
