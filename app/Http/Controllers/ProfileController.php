d58dc5a868c3fbe72fe032d0a68ac42ac1f8523a
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-07-22 12:32:15 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -55,7 +55,9 @@ class ProfileController extends Controller
     public function update(Request $request, string $id)
     {
         $this->validate($request, [
-            'name'
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'address' => ['required', 'string', 'min:5', 'max:300'],
         ]);
     }
 
