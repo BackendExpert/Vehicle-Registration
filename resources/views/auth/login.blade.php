49d047d11d59722a83c71bc5f99190e7e739eed9
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-07-22 09:02:33 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -56,7 +56,7 @@
 
 
                             <label for="password" class="label-text">{{__('Password')}}</label><br>
-                            <input type="password" id="password" class="form-input @error('password') is-invalid @enderror">
+                            <input type="password" id="password" class="form-input @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
                             <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
                         </form>
