eac05a944c6d6bd6ea60b37454efc306ce20fe55
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-07-22 13:15:28 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\VehicleController;
 
 /*
 |--------------------------------------------------------------------------
@@ -45,4 +46,4 @@ Route::resource("/users", UserController::class);
 
 Route::resource("/profile", ProfileController::class);
 
-Route::resource("/vehicles")
+Route::resource("/vehicles", VehicleController::class);
