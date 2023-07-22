598e2f872a20b87764abf114deaa68d7ea7c65c3
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-07-22 15:41:31 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- resources/views/vehicles/show.blade.php
+++ resources/views/vehicles/show.blade.php
@@ -16,5 +16,6 @@
         <b>Vehicle Type</b> : {{ $view_vehicle->vehicle_type }} <br>
         <b>Vehicle Model</b> : {{ $view_vehicle->vehicle_model }} <br>
         <b>Vehicle Color</b> : {{ $view_vehicle->vehicle_color }} <br>
+        <b>Seats</b> : {{ $view_vehicle->seats }}
     </div>
 @endsection
