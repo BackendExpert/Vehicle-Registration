ca77d0f84f1c139917e391a62232167a49ba2626
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-07-22 12:07:25 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

---
+++ resources/views/profile/edit.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.dash')
+
+@if (Auth::user()->id != $member_profile->id)
+<script>window.location = "/users";</script>
+@endif
+
+@section('content')
+    <div class="admin-card">
+        <h3>Update</h3>
+    </div>
+@endsection
