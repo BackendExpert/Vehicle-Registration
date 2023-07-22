2c0eaf1624f8ff8ad5bb68645c37ab2260cedcbe
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-07-22 15:33:05 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- resources/views/vehicles/show.blade.php
+++ resources/views/vehicles/show.blade.php
@@ -5,5 +5,7 @@
         <a href="{{ url('/vehicles') }}"><button class="btn btn-primary">Back</button></a>
         <hr>
         <br><br>
+
+        <h2>Vehicale Number {{ $ }}</h2>
     </div>
 @endsection
