3c476a86b3414b2ecea2241f7db7646c1d713d3b
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-07-22 14:07:11 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- resources/views/vehicles/create.blade.php
+++ resources/views/vehicles/create.blade.php
@@ -3,5 +3,6 @@
 @section('content')
     <div class="admin-card">
         <a href="{{ url('/vehicles') }}"><button class="btn btn-primary">Back</button></a>
+        <h2><i class=""></i> Register New Vehicle</h2>
     </div>
 @endsection
