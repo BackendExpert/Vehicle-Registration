07e7be9ac8c716180c498a7973b364fda5ac7f8b
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-07-22 11:52:45 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- app/Models/Member.php
+++ app/Models/Member.php
@@ -9,6 +9,6 @@ class Member extends Model
 {
     protected $table = 'users';
     protected $primaryKey = 'id';
-    protected $fillable = ['name', 'email', 'role', 'address', ''];
+    protected $fillable = ['name', 'email', 'role', 'address', 'created_at', 'updated_at'];
     use HasFactory;
 }
