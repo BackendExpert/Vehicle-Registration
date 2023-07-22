4a2a6c318bf440ed8cf5766a37218fe79f210ce6
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-07-22 13:11:05 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- app/Models/VehiclesM.php
+++ app/Models/VehiclesM.php
@@ -7,5 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class VehiclesM extends Model
 {
+    protected $table = 'users';
+    protected $primaryKey = 'id';
+    protected $fillable = ['name', 'email', 'role', 'address', 'created_at', 'updated_at'];
     use HasFactory;
 }
