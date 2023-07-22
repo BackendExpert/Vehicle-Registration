1ba3c6beae2f7e8c9c93e44b992f63515eaf5435
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-07-22 13:57:40 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- resources/views/vehicles/index.blade.php
+++ resources/views/vehicles/index.blade.php
@@ -7,5 +7,14 @@
 
         <a href="{{ url('vehicles/create') }}"><button class="btn btn-success"><i class="fas fa-plus"></i> Register New Vehicle</button></a>
         
+        <br><br>
+        <table class="table">
+            <thead class="table-primary">
+                <tr>
+                    <th>#</th>
+                </tr>
+            </thead>
+        </table>
+
     </div>
 @endsection
