39f9720e748b78ced34eed83d7533be13c09010b
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-07-22 15:34:32 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- resources/views/vehicles/show.blade.php
+++ resources/views/vehicles/show.blade.php
@@ -3,9 +3,10 @@
 @section('content')
     <div class="admin-card">
         <a href="{{ url('/vehicles') }}"><button class="btn btn-primary">Back</button></a>
-        <hr>
         <br><br>
 
         <h2>Vehicale Number {{ $view_vehicle->vehicle_plate_no }} Information</h2>
+        <hr>
+        <br>
     </div>
 @endsection
