45c1e3ae64debde7cb2cb61fa327b9fcb6c22c9f
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-07-22 15:56:19 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- resources/views/vehicles/show.blade.php
+++ resources/views/vehicles/show.blade.php
@@ -1,1 +1,11 @@
 @extends('layouts.header')
+
+<div class="container">
+    <div class="row">
+        <div class="col-lg-4"></div>
+        <div class="col-lg-4">
+            <h1>Online Vehicle Registration Application</h1>
+        </div>
+        <div class="col-lg-4"></div>
+    </div>
+</div>
