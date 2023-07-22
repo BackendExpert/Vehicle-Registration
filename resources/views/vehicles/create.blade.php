57c29e2a3f379f750016aa7e660ca3ec78dea4af
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-07-22 14:25:50 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- resources/views/vehicles/create.blade.php
+++ resources/views/vehicles/create.blade.php
@@ -39,6 +39,10 @@
                     <label for="regNo">Vehicle Registered Number : </label>
                     <input type="text" name="vehicle_registed_no" id="vehicle_registed_no" class="form-control">
                 </div>
+                <div class="col-lg-6">
+                    <label for="chassisNo">Vehicle Chassis Number : </label>
+                    <input type="text" name="vehicle_chassis_no" id="vehicle_chassis_no" class="form-control">
+                </div>
             </div>            
             <br><br>
             <div class="row">
