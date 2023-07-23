d326046c0c390db8be76656143d35b93434f2d86
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-07-23 11:13:41 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- database/migrations/2023_07_23_111322_create_vehicles_table.php
+++ database/migrations/2023_07_23_111322_create_vehicles_table.php
@@ -13,6 +13,19 @@ return new class extends Migration
     {
         Schema::create('vehicles', function (Blueprint $table) {
             $table->id();
+            $table->longText('frant_ing');
+            $table->longText('back_ing');
+            $table->longText('left_ing');
+            $table->longText('right_ing');
+            $table->longText('in_ing');
+            $table->string('vehicle_plate_no');
+            $table->string('vehicle_chassis_no');
+            $table->string('vehicle_registed_no');
+            $table->string('owner');
+            $table->string('vehicle_type');
+            $table->string('vehicle_model');
+            $table->string('vehicle_color');
+            $table->integer('seats');
             $table->timestamps();
         });
     }
