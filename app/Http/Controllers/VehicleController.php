3142436b2926a574cc1d494491fd029d6182d835
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-07-23 11:40:29 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- app/Http/Controllers/VehicleController.php
+++ app/Http/Controllers/VehicleController.php
@@ -50,8 +50,8 @@ class VehicleController extends Controller
             $frant_img_path = $request->file('frant_ing')->store('frant_ing', ['disk' => 'my_files']);
             $new_vechicle->frant_ing = $frant_img_path;
         }
-        if($request->hasFile('frant_ing')){
-            $back_img_path = $request->file('frant_ing')->store('frant_ing', ['disk' => 'my_files']);
+        if($request->hasFile('back_ing')){
+            $back_img_path = $request->file('back_ing')->store('back_ing', ['disk' => 'my_files']);
             $new_vechicle->frant_ing = $frant_img_path;
         }
         if($request->hasFile('frant_ing')){
