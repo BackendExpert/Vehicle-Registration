44ba37655441cb2c986b566b213b83bf553ddd98
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-07-23 11:26:43 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- resources/views/vehicles/create.blade.php
+++ resources/views/vehicles/create.blade.php
@@ -32,6 +32,7 @@
                 <div class="col-lg-4"></div>
                 <div class="col-lg-4"></div>
             </div>
+            <br><br>
 
             <div class="row">
                 <div class="col-lg-6">
