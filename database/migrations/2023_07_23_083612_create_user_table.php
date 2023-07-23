07bcfd93315efbb47b253d3418341354ac33d41c
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-07-23 08:38:22 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- database/migrations/2023_07_23_083612_create_user_table.php
+++ database/migrations/2023_07_23_083612_create_user_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->longText('profile_img');
+            $table->longText('profile_img')->default(0);
             $table->string('name');
             $table->string('email')->unique();
             $table->longText('address')->default(0);
