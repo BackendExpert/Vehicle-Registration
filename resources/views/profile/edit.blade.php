37ab3f96ce43c1dcda3a3b13c6ead51fb4f330fc
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-07-23 09:37:17 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -41,7 +41,7 @@
                 <div class="col-lg-4"></div>
                 <div class="col-lg-4">
                     <label for="profile_img">Profile Image : </label><br>
-                    <img src="{{ URL::asset("$member_profile->profile_img") }}" alt="Profile Image" >
+                    <img src="{{ URL::asset("$member_profile->profile_img") }}" alt="Profile Image" class="profile-img-view">
                     <input type="file" name="profile_img" id="profile_img" class="form-control">
                 </div>
                 <div class="col-lg-4"></div>
