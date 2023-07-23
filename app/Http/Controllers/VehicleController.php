cddb81913a8c13fe7a2d9d7fe23bee44e0e40e99
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-07-23 11:36:20 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- app/Http/Controllers/VehicleController.php
+++ app/Http/Controllers/VehicleController.php
@@ -50,7 +50,7 @@ class VehicleController extends Controller
         $new_vechicle->back_ing = $request->back_ing;
         $new_vechicle->left_ing = $request->left_ing;
         $new_vechicle->right_ing = $request->right_ing;
-        $new_vechicle->frant_ing = $request->frant_ing;
+        $new_vechicle->in_ing = $request->in_ing;
         $new_vechicle->vehicle_plate_no = $request->vehicle_plate_no;
         $new_vechicle->vehicle_chassis_no = $request->vehicle_chassis_no;
         $new_vechicle->vehicle_registed_no = $request->vehicle_registed_no;
